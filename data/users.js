0ac0eb32f257a410add7ce4cdc58427fd0dfc81f
Uqyanzie Bintang <uqyanzie@gmail.com>
2023-10-10 19:53:11 +0700

    user & auth

---
+++ data/users.js
@@ -0,0 +1,5 @@
+const Users = [
+    { id: "111", name: "Admin", email: "adminstaff@mail.com", password: 'admin123' },
+    { id: "112", name: "User", email: "useremail@mail.com", password: 'user123' },
+];
+export { Users };
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,76 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
+export const authOptions = {
+    secret: process.env.NextAuth_SECRET,
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "text",
+                    placeholder: "Enter email",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                    placeholder: "Enter Password",
+                },
+            },
+
+            async authorize(credentials, req) {
+                const { email, password } = credentials
+                //console.log(process.env.NEXTAUTH_URL)
+                const res = await fetch(`${process.env.NEXTAUTH_URL}/login`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email,
+                        password,
+                    }),
+                });
+                const user = await res.json();
+                if (res.ok && user) {
+                    return user;
+                } else return null;
+            },
+        }),
+        // ...add more providers here
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            return { ...token, ...user };
+        },
+        async session({ session, token, user }) {
+            // Send properties to the client, like an access_token from a provider.
+            session.user = token;
+            return session;
+        },
+        async redirect({ url, baseUrl }) {
+            // Allows relative callback URLs
+            if (url.startsWith("/")) return `${baseUrl}${url}`
+            // Allows callback URLs on the same origin
+            else if (new URL(url).origin === baseUrl) return url
+            return baseUrl
+        }
+        
+    },
+    pages: {
+        signIn: '/auth/signin',
+    },
+    
+}
+export default NextAuth(authOptions)
